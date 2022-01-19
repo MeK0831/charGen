20b0280d07622ce706064483faa194099c0d1ef6
MeK0831 <gloria0024@outlook.com>
2022-01-20 00:11:32 +0900

    20220120

---
+++ charGen.py
@@ -0,0 +1,21 @@
+#===== DB bellow =======
+
+#species
+species = {'mankind': ['human','giant','hobbit','valkyria','elf','darkelf','witch','clone'],
+           'fairykind':['seirei','dryad','dwarf','fairy','banshes'],
+           'undeadkind':['zombies','ghoul','vampire','dullahan'],
+           'monsterkind':['troll','orc','oni','dokkebi','halfmonster','slime','tentacle'],
+           'furrykind':['kumiho','lizardman','furry','lamia','insectoid','mermaid','arachne','hapi','seiren','minotauros',
+                        'satyr','werewolf','kitty','bunny','doggo','dragonia','octopus'],
+           'godkind':['angel','fallenangel','demon','reaper','god','demigod','succubus','halfdemon'],
+           'machinekind':['cyborg','humanoid','furriroid'],
+           'servantkind':['servant'],
+           'alienkind':['alien']}
+
+#type and relation
+type = ['mesugaki','one','loli','imouto','mature','classmate']
+relation = ['osananajimi','rival','enemy','boss','subordinate','master','slave','colleague']
+
+#personality
+personality = ['yandere','tsundere','megadere','cool','cynical','active','hikikomori','nerd']
+
